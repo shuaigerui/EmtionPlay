1629164d1121a565bb1b41cb226df84ea1845e57
Rae <544469923@qq.com>
2026-05-18 13:54:10 +0800

    Initial Commit

---
+++ EmtionPlayRp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmtionPlayRp
+//
+//  Created by  mac on 2026/5/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
